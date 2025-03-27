8436f2594ecd349ad4ff7b6ec490acd886b1d4f2
DaanStudent <d.aarts2@student.avans.nl>
2025-03-27 13:38:01 +0100

    Deur systeem toegevoegd met code en de getekende deuren toegevoegd

---
+++ Assets/Scripts/DoorController.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    public GameObject doorclosed;
+    public GameObject doorOpen;
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+         if (other.CompareTag("Player"))
+        {
+            Debug.Log("Door open!");
+            doorOpen.SetActive(true);
+            doorclosed.SetActive(false);
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Door closed!");
+            doorOpen.SetActive(false);
+            doorclosed.SetActive(true);
+        }
+    }
+}
